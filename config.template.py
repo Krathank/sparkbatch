880affc80fd57ecd5bb73b1bab0e64d185458f4a
Krathank <56928188+Krathank@users.noreply.github.com>
2020-11-30 21:03:44 +0530

    Update config.template.py
    updated config file

--- config.template.py
+++ config.template.py
@@ -1,10 +1,10 @@
-STORAGE_ACCOUNT_NAME = "datamindeddemodata"
-STORAGE_ACCOUNT_KEY = ""
+STORAGE_ACCOUNT_NAME = "testmyspark"
+STORAGE_ACCOUNT_KEY = "LFnaZBEdmBCTSRHwDU46pSNJ+rWI6OQGkYq7QcNZO5H6qdWSmzB+u3tjhp2j27OidQ1EINijAwDwSZMcXNeuAA=="
 
-BATCH_ACCOUNT_NAME = "dataminteddemobatch"
-BATCH_ACCOUNT_KEY = ""
-BATCH_ACCOUNT_URL = "https://dataminteddemobatch.westeurope.batch.azure.com"
+BATCH_ACCOUNT_NAME = "testmyspark"
+BATCH_ACCOUNT_KEY = "9vjBsJi8Z02NaKmPUE7WXxyzp7ehezvlhWv0aEd9crwVXiQQirpEJXirnSQqkxxu4z20sSnhEfrPbtGNveVbKg=="
+BATCH_ACCOUNT_URL = "https://testmyspark.westus.batch.azure.com"
 
-ACR_LOGINSERVER = "datamindeddemo.azurecr.io"
-ACR_USERNAME = "datamindeddemo"
-ACR_PASSWORD = ""
+ACR_LOGINSERVER = "testmyspark.azurecr.io"
+ACR_USERNAME = "testmyspark"
+ACR_PASSWORD = "eZNaY6jFir8U+innWIMhP4qK1sfqrYWG"
